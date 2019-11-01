b0e132e3532b7a1451f5f400155ec52f5c4ba8f5
Ian Cruz <iankruz98@gmail.com>
2019-11-02 04:36:41 +1100

    removes "[HorribleSubs]" from name will fail if cannot find Horriblesubs in name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+directory = r"C:\Users\iankr\Documents\Kens Folder\Projects\HorribleReNamer\Videos"
 
 
 
@@ -7,6 +8,20 @@ import os
 def main():
     print("Main Function")
 
+    for f in os.listdir(directory):
+        file_name, file_ext = os.path.splitext(f)
+        h_sub, f_title = file_name.split('] ')
+
+        #Get all necessary file paths
+        old_name = '{}\{}{}'.format(directory, file_name, file_ext)
+        new_name = '{}\{}{}'.format(directory, f_title, file_ext)
+        os.rename(old_name,new_name)
+
+
+
+
+
+    print("End Of File!")
 
 
 
