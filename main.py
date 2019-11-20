35cc40e6fbf1457f0d150154fb62ec0f95d18c62
ikruz98 <iankruz98@gmail.com>
2019-11-20 23:59:25 +1100

    Comment through code

--- main.py
+++ main.py
@@ -14,20 +14,22 @@ directory = os.getcwd()
 
 
 def main():
+    
     print("Main Function")
 
+    # walk through current directory of file
     for f in os.listdir(directory):
-        file_name, file_ext = os.path.splitext(f)
+        file_name, file_ext = os.path.splitext(f)       #split name from extension (filename & .mkv)
         
         try:
-            h_sub, f_title = file_name.split('] ')
+            h_sub, f_title = file_name.split('] ')      #seperate [HorribleSubs] from name
         
             #Get all necessary file paths
-            old_name = '{}\{}{}'.format(directory, file_name, file_ext)
-            new_name = '{}\{}{}'.format(directory, f_title, file_ext)
-            os.rename(old_name,new_name)
+            old_name = '{}\{}{}'.format(directory, file_name, file_ext) #Store old name
+            new_name = '{}\{}{}'.format(directory, f_title, file_ext)   #Create New Name
+            os.rename(old_name,new_name)                                #Rename
         except:
-            print("ERROR: Name does not match")
+            print("ERROR: Name does not match")                         #Will Fail and print this if filename does not contain [HorribleSubs]
 
 
 
