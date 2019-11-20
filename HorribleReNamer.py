7264c6db7914b8cba77883856842d678234fd2e9
ikruz98 <iankruz98@gmail.com>
2019-11-20 23:57:17 +1100

    -Renamed file to more appropriate name -Added instructions to top of file

--- HorribleReNamer.py
+++ HorribleReNamer.py
@@ -1,11 +1,3 @@
-#Batch Renames Horrible subs videos to remove "[HorribleSubs]" in file name
-#Instructions
-#   1. Make sure python is installed in your computer
-#   2. Copy and Paste this file into target folder
-#   3. Right click and select "Open with" then select python
-#   4. Move to next folder then just double click
-
-
 import os
 
 directory = os.getcwd()
