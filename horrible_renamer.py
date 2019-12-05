195a2b2e54c49c360b5c130810815004599125a9
ikruz98 <iankruz98@gmail.com>
2019-12-06 03:41:32 +1100

    Added os.walk functionality to only find  [Hsubs] tag

--- horrible_renamer.py
+++ horrible_renamer.py
@@ -8,48 +8,68 @@
 
 import os
 
-directory = os.getcwd()
+directory = "M:\Media\Anime"
+
+
 
 
 
 
 def main():
     
-    user_input = input("1. HorribleSubs \n2. Kaya \n3. Cleo \n4. Custom ")
-
-    # Preset selections 
-    try:
-        val = int(user_input)
-        print("Input is {}".format(user_input))
-        if (val == 1):
-            title = "HorribleSubs"
-        elif (val == 2):
-            title = "Kaya"
-        elif (val == 3):
-            title = "Cleo"
-        elif (val == 4):
-            title = input("Input Text to replace: ")
-    except ValueError:
-        print("Not a valid input! Try again")
+     
+  
 
 
 
 
 
+    # # Preset selections 
+    # try:
 
-    # walk through current directory of file
-    for f in os.listdir(directory):
-        file_name, file_ext = os.path.splitext(f)       #split name from extension (filename & .mkv)
+    #     user_input = input("1. HorribleSubs \n2. Kaya \n3. Cleo \n4. Custom ")
+
+    #     val = int(user_input)
+    #     print("Input is {}".format(user_input))
+    #     if (val == 1):
+    #         title = "HorribleSubs"
+    #     elif (val == 2):
+    #         title = "Kaya"
+    #     elif (val == 3):
+    #         title = "Cleo"
+    #     elif (val == 4):
+    #         title = input("Input Text to replace: ")
+    # except ValueError:
+    #     print("Not a valid input! Try again")
+
+
+
+    #  Try Walking through folders
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            if ("[HorribleSubs]" in name):
+                # print (name)
+                file_name, file_ext = os.path.splitext(name)
+                print("FileName: " + file_name)
+                print ("FileExt: " + file_ext)
+
+ 
+
+    
+
+    # # walk through current directory of file
+    # for f in os.listdir(directory):
+    #     file_name, file_ext = os.path.splitext(f)       #split name from extension (filename & .mkv)
         
-        try:
-            h_sub, f_title = file_name.split('[{}]'.format(title))      #seperate [HorribleSubs] from name
+    #     try:
+    #         h_sub, f_title = file_name.split('[{}]'.format(title))      #seperate [HorribleSubs] from name
         
-            #Get all necessary file paths
-            old_name = '{}\{}{}'.format(directory, file_name, file_ext) #Store old name
-            new_name = '{}\{}{}'.format(directory, f_title, file_ext)   #Create New Name
-            os.rename(old_name,new_name)                                #Rename
-        except:
-            print("ERROR: Name does not match")                         #Will Fail and print this if filename does not contain [HorribleSubs]
+    #         #Get all necessary file paths
+    #         old_name = '{}\{}{}'.format(directory, file_name, file_ext) #Store old name
+    #         new_name = '{}\{}{}'.format(directory, f_title, file_ext)   #Create New Name
+    #         os.rename(old_name,new_name)                                #Rename
+    #     except:
+    #         print("ERROR: Name does not match")                         #Will Fail and print this if filename does not contain [HorribleSubs]
 
 
 
